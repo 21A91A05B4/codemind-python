72454639a5752904c73e1ea18ec9d02c4e6f9458
21A91A05B4 <97424373+21A91A05B4@users.noreply.github.com>
2022-06-27 22:27:26 +0530

    def cntwords(str):     vowel=set('aeiouAEIOU')     c=0     for i in str.split():         i=list(i)         n=len(i)-1         if i[0] in vowel and i[n] not in vowel:             c=c+1     return c str=input() print(cntwords(str))

---
+++ count_words.py
@@ -0,0 +1,12 @@
+def cntwords(str):
+    vowel=set('aeiouAEIOU')
+    c=0
+    for i in str.split():
+        i=list(i)
+        n=len(i)-1
+        if i[0] in vowel and i[n] not in vowel:
+            c=c+1
+    return c
+str=input()
+print(cntwords(str))
+    
