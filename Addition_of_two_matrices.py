56bd99729521d480dd87dbd1b08a9817d7e8996a
21A91A05B4 <97424373+21A91A05B4@users.noreply.github.com>
2022-06-28 20:30:21 +0530

    import math r=int(input()) a=[list(map(int,input().split()))for i in range(r)] b=[list(map(int,input().split()))for i in range(r)] for i in range(r):     s=[]     for j in range(r):         s.append(a[i][j]+b[i][j])     print(*s)

--- Addition_of_two_matrices.py
+++ Addition_of_two_matrices.py
@@ -1,6 +1,7 @@
+import math
 r=int(input())
-a=[list(map(int,input().split())) for i in range(r)]
-b=[list(map(int,input().split())) for i in range(r)]
+a=[list(map(int,input().split()))for i in range(r)]
+b=[list(map(int,input().split()))for i in range(r)]
 for i in range(r):
     s=[]
     for j in range(r):
