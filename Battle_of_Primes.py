6450953b5e17a25f0c128d31a50c9e4e45dbc97f
21A91A05B4 <97424373+21A91A05B4@users.noreply.github.com>
2022-06-28 20:03:59 +0530

    def prime(n):     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False     else:         return True def fun(s):     for i in range(1,20):         t=s+i         if(prime(t)):             return i
    a=int(input())
    b=int(input())
    s=a+b
    k=fun(s)
    print(k)

--- Battle_of_Primes.py
+++ Battle_of_Primes.py
@@ -1,18 +1,17 @@
-def prime(t):
-    for j in range(2,int(t**0.5)+1):
-        if(t%j)==0:
-            return 0
+def prime(n):
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
     else:
-        return 1
+        return True
 def fun(s):
     for i in range(1,20):
         t=s+i
         if(prime(t)):
             return i
-    
+
 a=int(input())
 b=int(input())
-s=0
 s=a+b
 k=fun(s)
 print(k)
