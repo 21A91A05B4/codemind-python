c66cb2b43974c2de6df33ed85cb874df924ba474
21A91A05B4 <97424373+21A91A05B4@users.noreply.github.com>
2022-07-17 07:58:37 +0530

    s=input() s=s.lower() d=[] e=[] v=[] f=0 for i in s:     if i==' ':         continue     d.append(ord(i)) e=set(d) for i in e:     v.append(i) v.sort() for i in v:     print(chr(i),end='')

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,13 +1,16 @@
 s=input()
 s=s.lower()
 d=[]
+e=[]
+v=[]
 f=0
 for i in s:
     if i==' ':
         continue
-    if(s.count(i)==1):
-        d.append(ord(i))
-        
-d.sort()
-for i in d:
+    d.append(ord(i))
+e=set(d)
+for i in e:
+    v.append(i)
+v.sort()
+for i in v:
     print(chr(i),end='')
