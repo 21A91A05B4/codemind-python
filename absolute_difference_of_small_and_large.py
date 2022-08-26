59bc1bc3ff0ec657da8c35fa65cb59e07994de44
21A91A05B4 <97424373+21A91A05B4@users.noreply.github.com>
2022-08-26 10:02:08 +0530

    s=input() s=s.split() b=[] l='' a=[] for i in range(len(s)):     b.append(s[i]) for i in range(len(b)):     l=b[i]     for k in range(len(l)):         a.append(ord(l[k]))     m=min(a)     n=max(a)     print(n-m,end=' ')     l=''     a=[]     i+=1

--- absolute_difference_of_small_and_large.py
+++ absolute_difference_of_small_and_large.py
@@ -1,8 +1,27 @@
 s=input()
-d=[]
-for i in s.split():
-    d.append(i)
-for i in d:
-    m=ord(min(i))
-    n=ord(max(i))
-    print(abs(m-n),end=' ')
+s=s.split()
+b=[]
+l=''
+a=[]
+for i in range(len(s)):
+    b.append(s[i])
+for i in range(len(b)):
+    l=b[i]
+    for k in range(len(l)):
+        a.append(ord(l[k]))
+    m=min(a)
+    n=max(a)
+    print(n-m,end=' ')
+    l=''
+    a=[]
+    i+=1
+    
+        
+    
+    
+    
+    
+    
+    
+    
+    
