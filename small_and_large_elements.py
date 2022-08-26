662461d671187bb0ff5e554af75f53ca886deb4e
21A91A05B4 <97424373+21A91A05B4@users.noreply.github.com>
2022-08-26 10:13:58 +0530

    s=input() s=s.split() a=[] b=[] for i in range(len(s)):     b.append(s[i])     break for i in (s[::-1]):     a.append(i)     break l='' x='' for i in b:     l=i for i in a:     x=i a=[] b=[] for i in range(len(l)):     b.append(ord(l[i])) for i in range(len(x)):     a.append(ord(x[i])) print(chr(min(b)),end=' ') print(chr(max(a)))

--- small_and_large_elements.py
+++ small_and_large_elements.py
@@ -1,5 +1,24 @@
-s1=input()
-d=[]
-for i in s1.split():
-        d.append(i)
-print(min(d[0]),max(d[-1]),end=' ')
+s=input()
+s=s.split()
+a=[]
+b=[]
+for i in range(len(s)):
+    b.append(s[i])
+    break
+for i in (s[::-1]):
+    a.append(i)
+    break
+l=''
+x=''
+for i in b:
+    l=i
+for i in a:
+    x=i
+a=[]
+b=[]
+for i in range(len(l)):
+    b.append(ord(l[i]))
+for i in range(len(x)):
+    a.append(ord(x[i]))
+print(chr(min(b)),end=' ')
+print(chr(max(a)))
